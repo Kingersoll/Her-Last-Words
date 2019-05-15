3c699b2f1ae3dfac4e7408bb6eb1431af2bd27ad
Colin Everton <ceverton@student.fitchburgstate.edu>
2019-05-15 00:39:57 -0400

    push me harder

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class Grid : MonoBehaviour
 {
-
+    public bool displayGridGizmos;
     public LayerMask unwalkableMask;
     public Vector2 gridWorldSize;
     public float nodeRadius;
@@ -82,21 +82,17 @@ public class Grid : MonoBehaviour
         return grid[x, y];
     }
 
-    public List<Node> path;
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
-        if (grid != null)
+        if (grid != null && displayGridGizmos)
         {
             foreach (Node n in grid)
             {
                 Gizmos.color = (n.walkable) ? Color.white: Color.red;
-                if (path != null)
-                    if (path.Contains(n))
-                        Gizmos.color = Color.green;
                 Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
             }
 
